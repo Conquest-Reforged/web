3b7ea55fdd6d2598fd094022902811ae7c5cdc40
dags <dags-@users.noreply.github.com>
2017-12-11 10:03:02 +0000

    Update ghrelease.js

--- releases/ghrelease.js
+++ releases/ghrelease.js
@@ -1,3 +1,46 @@
+class ReleaseSection {
+  constructor(user, repo) {
+    this.user = user;
+    this.repo = repo;
+    this.title = (div, release) => {
+      div.innerText = `Latest: ${release.version}`;
+    };
+    this.button = (div, asset) => {
+      div.innerText = `${asset.name} (${asset.downloadCount} downloads)`;
+    };
+  }
+
+  load() {
+    let containers = document.getElementsByClassName('release-container');
+    if (containers === undefined || containers.length !== 1) {
+      console.warn('Error, page should container 1 "release-container" div');
+      return;
+    }
+
+    let container = containers[0];
+    let request = new ReleaseRequest(this.user, this.repo);
+    request.callback = release => {
+      let titles = document.getElementsByClassName('release-title');
+      if (titles !== undefined && titles.length === 1) {
+        this.title(titles[0], release);
+      }
+
+      let links = document.createElement('div');
+      release.assets.forEach(asset => {
+        let link = document.createElement('a');
+        link.classList.add('release-button');
+        link.href = asset.download;
+        this.button(link, asset);
+        container.appendChild(link);
+      });
+
+      container.appendChild(links);
+    };
+
+    request.send();
+  }
+}
+
 // user: String - the github username
 // repo: String - the name of the github repository (owned by 'user')
 // callback: function(Release) - the callback that accepts the retreived Release object
